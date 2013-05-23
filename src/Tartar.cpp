77fb1ce8072644a7e71fff8b03ed8bddcd5fa135
CaterHatterPillar <CaterHatterPillar@gmail.com>
2013-05-23 13:53:08 +0200

    Set-up Tartar-project. Initial commit.

---
+++ src/Tartar.cpp
@@ -0,0 +1,12 @@
+#include <Tartar.h>
+
+Tartar::Tartar() {
+
+}
+Tartar::~Tartar() {
+	// Do nothing.
+}
+
+void Tartar::init() {
+
+}
---
+++ src/Tartar.h
@@ -0,0 +1,16 @@
+#ifndef TARTAR_TARTAR_H
+#define TARTAR_TARTAR_H
+
+namespace Tartar {
+	class Tartar {
+	public:
+		Tartar();
+		~Tartar();
+
+		void init();
+	protected:
+	private:
+	};
+}
+
+#endif //TARTAR_TARTAR_H
