b91a545c5935a40dc50aeb4f12218f8aa7a1fb2b
CaterHatterPillar <CaterHatterPillar@gmail.com>
2013-05-24 11:30:51 +0200

    Successfully archiving five demo-files into archive that 7-zip may unpack without formatting issues.

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,22 @@
 // Demo
 int main() {
 	bool successDemo = false;
+
+	std::string demoFiles[5];
+	demoFiles[0] = "../demo1.txt";
+	demoFiles[1] = "../demo2.txt";
+	demoFiles[2] = "../demo3.txt";
+	demoFiles[3] = "../demo4.txt";
+	demoFiles[4] = "../demo5.txt";
 	
 	std::string tarName = "../demo.tar";
 	Tartar::Tarchive* tartar = new Tartar::Tarchive(tarName.c_str());
 	successDemo = tartar->init(); // Call me to initialize tar archive.
 	if( successDemo==true ) {
-		std::string demoFile = "../demo.txt";
-		successDemo = tartar->tarchiveFile( demoFile.c_str() );
+		for( unsigned int i = 0; i<5 && successDemo==true; i++ ) {
+			std::string demoFile = demoFiles[i];
+			successDemo = tartar->tarchiveFile( demoFile.c_str() );
+		}
 		if( successDemo==true ) {
 			tartar->done(); // Call me to close tar archive.
 		}
